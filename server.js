5e513fd413110f0af5e8aa39d37528806a2a9a11
ryteone1 <rynlacy@yahoo.com>
2017-11-02 03:59:26 -0400

    added methodOverride in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
 
 var PORT = process.env.PORT || 3000;
 
@@ -9,6 +10,7 @@ var app = express();
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
