10e02953fc13968b17fc8c81ae5df3ddfcf8b85a
ryteone1 <rynlacy@yahoo.com>
2017-11-02 04:15:36 -0400

    more

--- config/connection.js
+++ config/connection.js
@@ -13,9 +13,16 @@ if (process.env.JAWSDB_URL) {
     database: "burgers_db"
   });
 };
-
   
-connection.connect();
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 
 var PORT = process.env.PORT || 3000;
 
@@ -10,7 +10,7 @@ var app = express();
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+// app.use(methodOverride('_method'));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
