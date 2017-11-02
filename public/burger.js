fed7a902bb0aa6fa45f627270cb7353a9f87640b
ryteone1 <rynlacy@yahoo.com>
2017-11-01 23:10:16 -0400

    updates

--- public/burger.js
+++ public/burger.js
@@ -25,15 +25,15 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       name: $("#ca").val().trim(),
       sleepy: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
         console.log("created new cat");
@@ -47,11 +47,11 @@ $(function() {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "DELETE",
     }).then(
       function() {
-        console.log("deleted cat", id);
+        console.log("devoured burger", id);
         // Reload the page to get the updated list
         location.reload();
       }
